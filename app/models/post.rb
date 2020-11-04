6fe1cdd826a565ed051fa4ac1826077b7c2c64c9
Mark <amwhow@gmail.com>
2020-11-04 18:21:12 +1100

    hashtag added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,20 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+  has_many :post_hash_tags
+  has_many :hashtags, through: :post_hash_tags
+
+  after_commit :create_hash_tags, on: :create
+
+  def create_hash_tags 
+    post = Post.find(self.id)
+    collect_hash_tags.each do |x| 
+      ht = Hashtag.create(name:x)
+      post.hashtags << ht
+    end
+  end
+
+  def collect_hash_tags 
+    content.to_s.scan(/#\w+/).map {|x| x.downcase.gsub("#", "")} 
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_015955) do
+ActiveRecord::Schema.define(version: 2020_11_04_060730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 2020_11_04_015955) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -53,6 +59,15 @@ ActiveRecord::Schema.define(version: 2020_11_04_015955) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "post_hash_tags", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "hashtag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hashtag_id"], name: "index_post_hash_tags_on_hashtag_id"
+    t.index ["post_id"], name: "index_post_hash_tags_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -76,6 +91,10 @@ ActiveRecord::Schema.define(version: 2020_11_04_015955) do
     t.string "education"
     t.string "work"
     t.text "bio"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -85,5 +104,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_015955) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "post_hash_tags", "hashtags"
+  add_foreign_key "post_hash_tags", "posts"
   add_foreign_key "posts", "users", on_delete: :cascade
 end
